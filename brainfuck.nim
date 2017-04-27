df1f1cfbcfa0af16d0184caec728ed72c91725c1
Cristi Anichitei <cristi.anichitei@gmail.com>
2017-04-27 21:35:00 +0300

    Bring back the Interpret proc

--- brainfuck.nim
+++ brainfuck.nim
@@ -33,7 +33,7 @@ proc Compile(code: string): PNimrodNode {.compiletime.} =
     else: discard
 
   result = statements[0]
-  
+
 
 macro CompileString*(code: string): stmt =
   ## Compiles the brainfuck `code` string into Nim code that reads from stdin
@@ -46,6 +46,35 @@ macro CompileFile*(filename: string): stmt =
   Compile staticRead(filename.strval)
 
 
-proc Mandelbrot = CompileFile "examples/mandelbrot.b"
+proc Interpret*(code: string) =
+  ## Interprets the input string as brainfuck code
+  ## Reading is done from stdin, writing is done to stdout 
+  var
+    tape = newSeq[char]()
+    codePos = 0
+    tapePos = 0
 
-Mandelbrot()
+  proc run(skip = false): bool =
+    while codePos >= 0 and codePos < code.len:
+      tape.add '\0'
+
+      if code[codePos] == '[':
+        inc codePos
+        let oldCodePos = codePos
+        while run(tape[tapePos] == '\0'):
+          codePos = oldCodePos
+      elif code[codePos] == ']':
+        return tape[tapePos] != '\0'
+      elif not skip:
+        case code[codePos]
+        of '+': xinc tape[tapePos]
+        of '-': xdec tape[tapePos]
+        of '>': inc tapePos
+        of '<': dec tapePos
+        of '.': stdout.write tape[tapePos]
+        of ',': tape[tapePos] = stdin.readChar
+        else: discard
+
+      inc codePos
+
+  discard run()
