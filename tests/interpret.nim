39e583ada0cafc1efc3f6ba9edfd23d775314b77
Cristi Anichitei <cristi.anichitei@gmail.com>
2017-04-27 22:54:54 +0300

    Use example/* instead of ../example/* for interpreter tests
    We run the tests from the project folder, so ../examples is not the path we want

--- tests/interpret.nim
+++ tests/interpret.nim
@@ -5,7 +5,7 @@ import unittest, ../src/brainfuck
 
 suite "brainfuck interpreter":
   test "interpret helloworld":
-    let helloworld = readFile("../examples/helloworld.b")
+    let helloworld = readFile("examples/helloworld.b")
     # We are actually returning "Hello World!\n", represented as "Hello World!\x0A",
     # but using "Hello World!\n" as the test strings creates "Hello World!\r\n" (ending in \x0D\x0A)
     # This might have another behaviour on non-Windows operating systems
@@ -13,7 +13,7 @@ suite "brainfuck interpreter":
 
 
   test "interpret rot13":
-    let rot13 = readFile("../examples/rot13.b")
+    let rot13 = readFile("examples/rot13.b")
     let conv = Interpret(rot13, "Secret\n")
     check conv == "Frperg\n"
     check Interpret(rot13, conv) == "Secret\n"
